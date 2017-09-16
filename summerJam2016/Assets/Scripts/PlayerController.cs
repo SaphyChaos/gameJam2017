7968e1290ca48d9374186317ce146744f227bde9
KatelynS <kate14@umbc.edu>
2017-09-16 18:14:53 -0400

    this hub really is a git

--- summerJam2016/Assets/Scripts/PlayerController.cs
+++ summerJam2016/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,30 @@ public class PlayerController : MonoBehaviour {
 
 	private float x;
 	public float speed = 0.5f;
-
+	public Transform respawn;
+	private float killFloor = -5.0f;
+	//private Touch tap;
 
 	// Use this for initialization
 	void Start () {
-		
+		killFloor = -5.0f;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		//tap = Input.GetTouch(0);//touches[0];
+		if (Input.GetMouseButtonDown(0))//GetTouch(0).tapCount > 0)
+			transform.position.y++;
+		if (killFloor < (transform.position.y - 5))
+			killFloor = transform.position.y - 5;
 		x = Input.acceleration.x * speed;
 		transform.Translate(x, 0, 0);
+
+		if (transform.position.y < -5)
+		{
+			this.transform.position = respawn.position; 	//respawn at begining
+			this.transform.rotation = respawn.rotation;
+		}
+		
 	}
 }
