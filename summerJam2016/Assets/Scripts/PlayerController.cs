8a79d1ab67620f1fddbc52b3218784d56b5c8c0b
KatelynS <kate14@umbc.edu>
2017-09-15 22:54:45 -0400

    Tilt
    got tilt controlls, phone gameplay, and player centered camera

---
+++ summerJam2016/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float x;
+	public float speed = 0.5f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		x = Input.acceleration.x * speed;
+		transform.Translate(x, 0, 0);
+	}
+}
