8ca0f5bdad652af3ca857eceaa11f20789a1690a
KatelynS <kate14@umbc.edu>
2017-09-16 00:42:36 -0400

    changes?
    all the things i did might be overwritten

--- summerJam2016/Assets/Scripts/CameraController.cs
+++ summerJam2016/Assets/Scripts/CameraController.cs
@@ -8,14 +8,11 @@ public class CameraController : MonoBehaviour {
 
 	private Vector3 offset;
 
-	void Start () 
-	{
+	void Start () {
 		offset = transform.position - player.transform.position;
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () 
-	{
+
+	void LateUpdate () {
 		transform.position = player.transform.position + offset;
 	}
 }
