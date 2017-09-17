f28bc30042947f2115951e777779c6b5573291de
KatelynS <kate14@umbc.edu>
2017-09-17 15:14:38 -0400

    last post! \o/

---
+++ Gravity testing/Gravity Test/Assets/Standard Assets/2D/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0)
+			//change scene to main game
+			Application.LoadLevel ("kitscene");
+	}
+}
