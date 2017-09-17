261b050fec87f1bf951f0f598f1c8a75cc6c03bc
KatelynS <kate14@umbc.edu>
2017-09-17 00:00:52 -0400

    superjump

--- Gravity testing/Gravity Test/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Gravity testing/Gravity Test/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -10,7 +10,13 @@ namespace UnityStandardAssets._2D
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
 
+		//rudimentary kill floor setup (KF)
+		public Transform respawn;
+		private float killFloor;
 
+		void Start () {
+			killFloor = -25.0f;
+		}
         private void Awake()
         {
             m_Character = GetComponent<PlatformerCharacter2D>();
@@ -22,16 +28,26 @@ namespace UnityStandardAssets._2D
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+				if (Input.touchCount > 0)
+					m_Jump = true;//CrossPlatformInputManager.GetButtonDown("Jump");
             }
+			//(KF)
+			if (killFloor < (transform.position.y - 25.0f))
+				killFloor = transform.position.y - 25.0f;
+			if (transform.position.y < killFloor)
+			{
+				this.transform.position = respawn.position; 	//respawn at beginning
+				this.transform.rotation = respawn.rotation;
+				killFloor = transform.position.y - 25.0f;
+			}
         }
 
 
         private void FixedUpdate()
         {
             // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			bool crouch = false;//Input.GetKey(KeyCode.LeftControl);
+			float h = Input.acceleration.x;//CrossPlatformInputManager.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
---
+++ Gravity testing/Gravity Test/Assets/cameraPull.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cameraPull : MonoBehaviour {
+	public GameObject myBoy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var otherPos = transform.position;
+		transform.position = new Vector3(otherPos.x,otherPos.y,(-13+myBoy.GetComponent<Rigidbody2D>().gravityScale));
+	}
+}
