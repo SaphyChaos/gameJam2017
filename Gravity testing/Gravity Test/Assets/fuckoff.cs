c0e4f155b83c9c2c2ea0be86ccfb55617fe59dc5
Kit Heckman <kit.heckman@gmail.com>
2018-06-13 15:52:27 -0400

    tiiles???

--- Gravity testing/Gravity Test/Assets/fuckoff.cs
+++ Gravity testing/Gravity Test/Assets/fuckoff.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class fuckoff : MonoBehaviour {
     private Rigidbody m_Rigidbody;
+    private Vector3 myVector;
+    private int i;
     // Use this for initialization
     void Start () {
         m_Rigidbody = GetComponent<Rigidbody>();
-        m_Rigidbody.AddForce(new Vector3(0f, 200, -400));
+        m_Rigidbody.AddForce(new Vector3(0f, 400, -400));
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        i -= 10;
+        myVector = new Vector3(0, 0, i);
+        Quaternion deltaRotation = Quaternion.Euler(myVector * Time.deltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+    }
 }
